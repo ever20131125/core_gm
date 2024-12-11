453b5134869aba0cc2e3ac2f0bdede787b869013
jian_yang <jian_yang@meta48.com>
2024-12-11 15:00:14 +0800

    test

--- src/Account/Account.php
+++ src/Account/Account.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model {
     protected $fillable = ['publisher', 'pfId', 'openid', 'uid', 'old_publisher', 'old_openId', 'old_pfId', 'type'];
 
-    public function __construct($connection = 'xnhd_base') {
+    public function __construct($connection = 'xnhd_base') {//test
         $this->setConnection($connection);
         // default table 'accounts'
         parent::__construct();
